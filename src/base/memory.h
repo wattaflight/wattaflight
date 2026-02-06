9df41ff1a34207989608cd24ad78e55e9d087c6a
Andrei Kiiko <eb08a167@proton.me>
2026-02-07 00:14:25 +0200

    Add basic support for FASTMEM* hints

---
+++ src/base/memory.h
@@ -0,0 +1,7 @@
+/*
+ * SPDX-FileCopyrightText:  2026 WattaFlight contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#define FASTMEMI __attribute__((section(".fastdata")))
+#define FASTMEM0 __attribute__((section(".fastbss")))
