5dd032e328214e1f6ca0ae935ed70d3ceda4d322
Andrei Kiiko <eb08a167@proton.me>
2026-02-14 17:55:43 +0200

    Rename cmsis -> pal

--- src/mcu/stm32/f4/mcu.mk
+++ src/mcu/stm32/f4/mcu.mk
@@ -1,4 +1,4 @@
 MCU_ARCH_FLAGS += -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -mfloat-abi=hard
-MCU_INCLUDES   += lib/cmsis_stm32f4/Include
+MCU_INCLUDES   += lib/stm32f4_pal/Include
 
 include src/mcu/stm32/f4/$(PLATFORM_LINE)/mcu.mk
--- src/mcu/stm32/h7/mcu.mk
+++ src/mcu/stm32/h7/mcu.mk
@@ -1,4 +1,4 @@
 MCU_ARCH_FLAGS += -mcpu=cortex-m7 -mfpu=fpv5-d16 -mfloat-abi=hard
-MCU_INCLUDES   += lib/cmsis_stm32h7/Include
+MCU_INCLUDES   += lib/stm32h7_pal/Include
 
 include src/mcu/stm32/h7/$(PLATFORM_LINE)/mcu.mk
